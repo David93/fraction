e76d5d08c64da641596e77472aaef061b5ad6165
David Joshy <david.joshy@gmail.com>
2017-07-09 13:18:49 -0400

    Added arithmetic ops

--- fraction.cpp
+++ fraction.cpp
@@ -26,20 +26,49 @@ class Fraction{
 		Fraction operator/(const Fraction& b);
 		bool operator>(const Fraction& b);
 		bool operator<(const Fraction& b);
+		bool operator==(const Fraction& b);
+		bool operator!=(const Fraction& b);
 		//reduce/simplify func
 		void reduce();
 		//display function
 		void display(){cout<<num<<"/"<<dem<<endl;}	
 };
 void Fraction::reduce(){
-	int h=HCF(num,dem);
+	int h=HCF(abs(num),abs(dem));
 	num/=h;
 	dem/=h;
-
+}
+Fraction Fraction::operator+(const Fraction& b){
+	int n1=(this->num)*(b.dem);
+	int n2=(this->dem)*(b.num);
+	int d=(this->dem)*(b.dem);
+	Fraction res(n1+n2,d);
+	res.reduce();
+	return res;
+}
+Fraction Fraction::operator-(const Fraction& b){
+	int n1=(this->num)*(b.dem);
+	int n2=(this->dem)*(b.num);
+	int d=(this->dem)*(b.dem);
+	Fraction res(n1-n2,d);
+	res.reduce();
+	return res;
+}
+Fraction Fraction::operator*(const Fraction& b){
+	int n=(this->num)*(b.num);
+	int d=(this->dem)*(b.dem);
+	Fraction res(n,d);
+	res.reduce();
+	return res;
+}
+Fraction Fraction::operator/(const Fraction& b){
+	return (*this)*(Fraction(b.dem,b.num));
 }
 int main(){
 	cout<<"Fraction Testbench"<<endl;
-	Fraction t(5,95);
+	Fraction a(1,4);
+	Fraction b(1,2);
+	Fraction t=a/b;
 	t.display();
 	return 0;
 }
