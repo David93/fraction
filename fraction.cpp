831f99a697e1daf5af655958f0bece2b13347e01
David Joshy <david.joshy@gmail.com>
2017-07-09 10:54:22 -0400

    added display and constructor

--- fraction.cpp
+++ fraction.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+using namespace std;
 class Fraction{
 	private:
-	int num, dem;	
+	int num, dem;
+	public:
+	Fraction(int n, int d):num(n),dem(d){}
+	void display(){cout<<num<<"/"<<dem<<endl;}	
 };
 int main(){
-	
+	cout<<"Fraction Testbench"<<endl;
+	Fraction t(1,2);
+	t.display();
 	return 0;
 }
