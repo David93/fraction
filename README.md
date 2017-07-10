547109e01103ee532dd61f19d79d605e6c37eaf8
David Joshy <david.joshy@gmail.com>
2017-07-09 20:29:44 -0400

    Added exp and inverse ops(^ && !)

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # fraction
 C++ class that supports fraction operations like +,-,/,*,<,>,==,!=,<<
 Also works with int and double types.
+Added exp and inverse ops(^ && !)
 
--- fraction.cpp
+++ fraction.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 using namespace std;
 //HCF func
 int HCF(int a, int b){
@@ -23,11 +24,11 @@ class Fraction{
 		Fraction(int n):num(n),dem(1){}
 		Fraction(double n){
 			string s=to_string(n);
-			int dot=s.find(".");
+			size_t dot=s.find(".");
 			int a=stoi(s.substr(0,dot));
 			int b=stoi(s.substr(dot+1,s.size()-dot));
-			while(b%10==0)
-				b/=10;
+			while(b%10==0 &&b)
+				{b/=10;}
 			dem=1;
 			num=stoi(to_string(a)+to_string(b));
 			while(b)
@@ -35,10 +36,15 @@ class Fraction{
 			reduce();
 		}
 		//overloaded ops
+		//arith ops
 		Fraction operator+(const Fraction& b);
 		Fraction operator-(const Fraction& b);
 		Fraction operator*(const Fraction& b);
 		Fraction operator/(const Fraction& b);
+		//exp and inverse op
+		Fraction operator^(const Fraction& b);
+		Fraction operator!();
+		//relational ops
 		bool operator>(const Fraction& b);
 		bool operator<(const Fraction& b);
 		bool operator==(const Fraction& b);
@@ -82,6 +88,18 @@ Fraction Fraction::operator*(const Fraction& b){
 Fraction Fraction::operator/(const Fraction& b){
 	return (*this)*(Fraction(b.dem,b.num));
 }
+Fraction Fraction::operator^(const Fraction& b){
+	double x=pow(this->num,double(b.num)/b.dem);
+	double y=pow(this->dem,double(b.num)/b.dem);
+	Fraction n(x);
+	Fraction d(y);
+	cout<<x<<'/'<<y<<endl;
+	return n/d;
+
+}
+Fraction Fraction::operator!(){
+	return Fraction(dem,num);
+}
 bool Fraction::operator<(const Fraction& b){
 	int n1=(this->num)*(b.dem);
 	int n2=(this->dem)*(b.num);
@@ -114,8 +132,9 @@ ostream& operator<<(ostream& os, const Fraction& f)
 }  
 int main(){
 	cout<<"Fraction Testbench"<<endl;
-	Fraction a(3,4);
-	Fraction b=a*1.25;
+	Fraction a(4,25);
+	Fraction b=a^1.5;
 	cout<<b<<endl;
+	cout<<!a<<endl;
 	return 0;
 }
